5718086152cebc3d92191306932caeab4b0156c3
Nancy Trinh <nancytrinh20@gmail.com>
2019-12-07 12:47:46 -0800

    Set up bootstrap and font awesome 5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "bootstrap";
+import "../stylesheets/application";
+import "./custom";
+import "@fortawesome/fontawesome-free/js/all";
+
+window.jQuery = $;
+window.$ = $;
